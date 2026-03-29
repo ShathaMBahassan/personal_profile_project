56be6892dc5d2550a343de11126973669f7cee42
ShathaMBahassan <92083449+Shathamhb@users.noreply.github.com>
2026-03-29 11:34:29 +0300

    Use bootstrap in aboutMe app

--- aboutMe/views.py
+++ aboutMe/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import aboutMe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'aboutMe\home.html')
+    about_me = aboutMe.objects.all().first()
+    context={'about_me':about_me}
+    return render(request, 'aboutMe\home.html',context)
