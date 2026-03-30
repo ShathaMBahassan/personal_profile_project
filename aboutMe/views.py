3b4a7867cd53b3338c874ba66ae52d9d6f821a1c
ShathaMBahassan <92083449+Shathamhb@users.noreply.github.com>
2026-03-30 10:57:36 +0300

    Add Certification objcets into a section

--- aboutMe/views.py
+++ aboutMe/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import aboutMe , Education
+from .models import aboutMe , Education , certificate
 
 # Create your views here.
 
 def home(request):
     about_me = aboutMe.objects.all().first()
     edu = Education.objects.filter(about_me=1)
-    context={'about_me':about_me , 'edu':edu}
+    cer = certificate.objects.filter(about_me=1)
+    context={'about_me':about_me , 'edu':edu , 'cer':cer}
     return render(request, 'aboutMe\home.html',context)
